1eece681a5612d6eafec3a93990c02c957e1f644
tahany <tahanymohammed777@hotmail.com>
2022-03-26 17:47:48 +0300

    fix

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getPosts, createPost } from '../controllers/posts.js';
+import { getPosts, createPost, updatePost } from '../controllers/posts.js';
 const router = express.Router();
 
 router.get('/', getPosts);
